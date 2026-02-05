c2ba1e71f558b137d4617e048fe37beaf9111ef4
Carlos Mondaca <carmond518@gmail.com>
2026-02-05 18:34:33 -0300

    Enhance Footer component by adding social media icons for Email and LinkedIn, improve layout for responsiveness, and update styling for better user experience. Include new SVG icons for Email and LinkedIn.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,26 @@
+import EmailIcon from "@/public/icons/email.svg";
+import LinkedInIcon from "@/public/icons/linkedin.svg";
 import Logo from "./Logo";
 import Button from "./ui/Button";
 
 export default function Footer() {
+  const socialMedia = [
+    {
+      name: "Email",
+      href: "mailto:contacto@isolegal.cl",
+      icon: <EmailIcon className="w-5.5 h-5.5 fill-[#64748B]" />,
+    },
+    {
+      name: "LinkedIn",
+      href: "https://www.linkedin.com/company/isolegal",
+      icon: <LinkedInIcon className="w-5.5 h-5.5 fill-[#64748B]" />,
+    },
+  ];
+
   return (
     <footer className="bg-white">
       <div className="container mx-auto py-16">
-        <div className="flex flex-row justify-between">
+        <div className="flex flex-col md:flex-row justify-between">
           <div>
             <h3 className="text-xl font-bold">
               Suscríbete a Radar Legislativo
@@ -18,11 +33,12 @@ export default function Footer() {
             <Button
               text="Suscribirme"
               href="https://www.linkedin.com/build-relation/newsletter-follow?entityUrn=7170086036545478656"
+              className="w-full md:w-auto mt-4 md:mt-0"
             />
           </div>
         </div>
         <div className="h-px bg-gray-200 my-6" />
-        <div className="">
+        <div className=" flex flex-col md:flex-row justify-between">
           <div>
             <Logo
               colors={{ primary: "#1B3C59", secondary: "#E33421" }}
@@ -34,21 +50,39 @@ export default function Footer() {
               <br />
               No Explicaciones
             </p>
+            <div className="flex flex-row gap-4 mt-4">
+              {socialMedia.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center"
+                >
+                  {item.icon}
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className="mt-4 md:mt-0">
+            <span className="text-md font-bold text-text">
+              Av. Bosques de Montemar N°30, Of. 316, Viña del Mar.
+            </span>
           </div>
         </div>
         <div className="h-px bg-gray-100 my-6" />
-        <div className="flex flex-row justify-between">
+        <div className="flex flex-col md:flex-row justify-between gap-4 md:gap-0">
           <span className="text-sm text-gray-500">
             &copy; {new Date().getFullYear()} Isolegal. Todos los derechos
             reservados.
           </span>
           <div className="flex flex-row gap-4 text-sm text-gray-500">
-            <span className="hover:text-gray-900 transition-colors cursor-pointer">
+            <a className="hover:text-gray-900 transition-colors cursor-pointer">
               Política de privacidad
-            </span>
-            <span className="hover:text-gray-900 transition-colors cursor-pointer">
+            </a>
+            <a className="hover:text-gray-900 transition-colors cursor-pointer">
               Términos y condiciones
-            </span>
+            </a>
           </div>
         </div>
       </div>
