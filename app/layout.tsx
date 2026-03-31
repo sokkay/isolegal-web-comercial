0f4a0feb8721d778d01aa3560520389c5e047629
Carlos Mondaca <carmond518@gmail.com>
2026-03-31 12:05:05 -0300

    informacion para google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,14 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import { PostHogProvider } from "@/lib/posthog/PostHogProvider";
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const manrope = Manrope({
   variable: "--font-manrope",
   subsets: ["latin"],
 });
+const GTM_ID = "GTM-K6KZ57WP";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://isolegal.cl"),
@@ -94,8 +96,25 @@ export default function RootLayout({
     <html lang="es" className={manrope.variable} suppressHydrationWarning>
       <head>
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
       </head>
       <body className="antialiased">
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <ThemeProvider>
           <ReactQueryProvider>
             <PostHogProvider>
