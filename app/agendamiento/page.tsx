2c553f59a4617582f6b37ce2d554ab8146ae719f
Carlos Mondaca <carmond518@gmail.com>
2026-04-16 15:11:52 -0400

    Add BussinessSection to Agendamiento and Contacto pages

--- app/agendamiento/page.tsx
+++ app/agendamiento/page.tsx
@@ -1,9 +1,11 @@
 import AgendamientoHeading from "@/sections/agendamiento/AgendamientoHeading";
+import BussinessSection from "@/sections/bussiness";
 
 export default function AgendamientoPage() {
   return (
     <main className="flex w-full mx-auto flex-col bg-background">
       <AgendamientoHeading />
+      <BussinessSection />
     </main>
   );
 }
--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,9 +1,11 @@
+import BussinessSection from "@/sections/bussiness";
 import HeadingSection from "@/sections/heading";
 
 export default function ContactoPage() {
   return (
     <main className="flex w-full mx-auto flex-col bg-background">
       <HeadingSection />
+      <BussinessSection />
     </main>
   );
 }
